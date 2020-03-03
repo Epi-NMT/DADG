5e37232549bbf78902a05650bd21285befbd2b4f
keyu07 <33266145+keyu07@users.noreply.github.com>
2020-03-03 10:23:40 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,30 +11,26 @@ def sgd(model_name, parameters, lr):
         opt = optim.SGD(params=parameters, lr=lr, weight_decay=5e-5, momentum=0.9)
 
     elif model_name == 'resnet18':
-        opt = optim.Adam(params=parameters, lr=lr)
+        opt = optim.SGD(params=parameters, lr=lr)
     return opt
+
+
+def lr_scheduler(total_epoch, epoch, lr):
+    step_size = int(total_epoch / 2)
+
+    if (epoch+1) <= step_size * 1:
+        return lr
+
+    elif(epoch+1) > step_size * 1:
+        learning_rate = 5e-6
+        return learning_rate
+
 
 def domain_sgd(model_name, parameters, lr):
     if model_name == 'alexnet':
         opt = optim.SGD(params=parameters, lr=lr)
 
     elif model_name == 'resnet18':
-        opt = optim.Adam(params=parameters, lr=lr)
+        opt = optim.SGD(params=parameters, lr=lr)
     return opt
-
-def lr_scheduler(total_ite, ite, lr):
-    step_size = int(total_ite / 3)
-
-    if (ite+1) <= step_size * 1:
-        return lr
-
-    elif(ite+1) > step_size * 1 and (ite+1) <= step_size * 2:
-        learning_rate = 5e-5
-        return learning_rate
-
-    elif (total_ite+1) > step_size * 2:
-        learning_rate = 5e-6
-        return learning_rate
-
-
 
