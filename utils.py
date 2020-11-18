11f290adfc04288ac0e752378a450f2826d207d4
keyu07 <33266145+keyu07@users.noreply.github.com>
2020-11-18 11:22:35 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ def sgd(model_name, parameters, lr):
     if model_name == 'alexnet':
         opt = optim.SGD(params=parameters, lr=lr, weight_decay=5e-5, momentum=0.9)
 
-    elif model_name == 'resnet50':
+    elif model_name == 'resnet18':
         opt = optim.SGD(params=parameters, lr=lr, weight_decay=5e-5, momentum=0.9)
 
     return opt
@@ -19,7 +19,7 @@ def domain_sgd(model_name, parameters, lr):
     if model_name == 'alexnet':
         opt = optim.SGD(params=parameters, lr=lr)
 
-    elif model_name == 'resnet50':
+    elif model_name == 'resnet18':
         opt = optim.SGD(params=parameters, lr=lr)
 
     return opt
