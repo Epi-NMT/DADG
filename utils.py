70d13a56be7b29fb74cdc073cdcb0da669d5af78
keyu07 <33266145+keyu07@users.noreply.github.com>
2020-11-18 11:22:11 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,36 +1,29 @@
 import torch
-from torch import optim, nn
-
+from torch import optim
 
 def loss_fn():
     loss_func = torch.nn.CrossEntropyLoss()
     return loss_func
 
+
 def sgd(model_name, parameters, lr):
     if model_name == 'alexnet':
         opt = optim.SGD(params=parameters, lr=lr, weight_decay=5e-5, momentum=0.9)
 
-    elif model_name == 'resnet18':
-        opt = optim.SGD(params=parameters, lr=lr)
+    elif model_name == 'resnet50':
+        opt = optim.SGD(params=parameters, lr=lr, weight_decay=5e-5, momentum=0.9)
+
     return opt
-
-
-def lr_scheduler(total_epoch, epoch, lr):
-    step_size = int(total_epoch / 2)
-
-    if (epoch+1) <= step_size * 1:
-        return lr
-
-    elif(epoch+1) > step_size * 1:
-        learning_rate = 5e-6
-        return learning_rate
-
 
 def domain_sgd(model_name, parameters, lr):
     if model_name == 'alexnet':
         opt = optim.SGD(params=parameters, lr=lr)
 
-    elif model_name == 'resnet18':
+    elif model_name == 'resnet50':
         opt = optim.SGD(params=parameters, lr=lr)
+
     return opt
+
+
+
 
